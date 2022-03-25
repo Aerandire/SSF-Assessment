345bcd7310d2dd206db86334d6b2397a3ab7ae8f
Keefe Tan Kui Yu <keefe@Keefes-MacBook-Pro.local>
2022-03-25 09:19:26 +0800

    setup task2

---
+++ src/main/java/ssf/assess/SSFAssessment/controller/PurchaseOrderRestController.java
@@ -0,0 +1,8 @@
+package ssf.assess.SSFAssessment.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PurchaseOrderRestController {
+    
+}
---
+++ src/main/java/ssf/assess/SSFAssessment/services/QuotationService.java
@@ -0,0 +1,8 @@
+package ssf.assess.SSFAssessment.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuotationService {
+    
+}
