3e783e55dcd05c12172fa435cf07e70cde3da31b
Keefe Tan Kui Yu <keefe@Keefes-MacBook-Pro.local>
2022-03-25 09:13:55 +0800

    First Commit

---
+++ src/main/java/ssf/assess/SSFAssessment/SsfAssessmentApplication.java
@@ -0,0 +1,13 @@
+package ssf.assess.SSFAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/assess/SSFAssessment/SsfAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.assess.SSFAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
